ce1eb14cce0cc9f26dda85653d9f10bdd7d1ccef
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-12-19 12:54:50 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  withCredentials: true, //  cookies (refresh token)
+  baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true,
 });
 
 export default api;
